7d3a6f2e63ceb7008b04ef86637502836314eb60
monoland <mono.socialmedia@gmail.com>
2019-11-11 19:18:20 +0700

    add sync control

---
+++ database/seeds/SyncControlTable.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Control;
+use App\Models\Item;
+use App\Models\Vehicle;
+use Illuminate\Database\Seeder;
+
+class SyncControlTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $vehicles = Vehicle::withCount('controls')->get();
+
+        foreach ($vehicles as $vehicle) {
+            if ($vehicle->controls_count > 0) {
+                continue;
+            }
+
+            $items = Item::where('kind', $vehicle->kind)->get();
+            $controls = [];
+
+            foreach ($items as $item) {
+                array_push(
+                    $controls, new Control([
+                        'item_id' => $item->id, 
+                        'year' => date('Y'),
+                        'maxi' => $item->maxi,
+                        'blnc' => $item->maxi
+                    ])
+                );
+            }
+
+            if (count($controls) > 0) {
+                $vehicle->controls()->saveMany($controls);
+            }
+        }
+    }
+}
