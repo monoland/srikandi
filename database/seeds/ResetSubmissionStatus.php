a74c835b930e3e8e484e9137f26faf9b9348322f
monoland <mono.socialmedia@gmail.com>
2019-11-19 11:45:16 +0700

    add reset submission

---
+++ database/seeds/ResetSubmissionStatus.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Service;
+use Illuminate\Database\Seeder;
+
+class ResetSubmissionStatus extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $services = Service::where('status', 'submissioned')->get();
+
+        foreach ($services as $service) {
+            $service->items()->newPivotStatement()->update(['exmn' => false]);
+        }
+    }
+}
